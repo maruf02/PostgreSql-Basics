0c2dd684d9d366e09d9521dbefe0b719186d95e2
maruf02 <rashiduzzaman99@gmail.com>
2024-10-27 19:12:35 +0600

    create student table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -3,3 +3,14 @@
 -- create databade named university_db.
 CREATE DATABASE university_db;
  
+ -- Creating student table
+
+ CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(50) NOT NULL,
+    age INTEGER NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    frontend_mark INTEGER,
+    backend_mark INTEGER,
+    status VARCHAR(20)
+);
