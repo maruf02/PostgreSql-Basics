5fdad24c015e0f74bb461800cc40ff4688622b4f
maruf02 <rashiduzzaman99@gmail.com>
2024-10-27 20:06:09 +0600

    Query 3,4 complete

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -77,3 +77,17 @@ FROM students s
 JOIN enrollment e ON s.student_id = e.student_id
 JOIN courses c ON e.course_id = c.course_id
 WHERE c.course_name = 'Next.js';
+
+
+
+-- Query 3: Update the status of the student with the highest total (frontend_mark + backend_mark) to 'Awarded'.
+UPDATE students
+SET status = 'Awarded'
+WHERE (frontend_mark + backend_mark) = (
+    SELECT MAX(frontend_mark + backend_mark) FROM students
+);
+
+
+-- Query 4: Delete all courses that have no students enrolled.
+DELETE FROM courses
+WHERE course_id NOT IN (SELECT DISTINCT course_id FROM enrollment);
